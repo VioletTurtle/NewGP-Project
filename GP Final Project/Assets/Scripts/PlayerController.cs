25204502b0eff2ac68fef23d37099eb7b2260d22
VioletTurtle <violetturtle2002@gmail.com>
2019-11-06 20:57:59 -0500

    New Project Fixed, DO NOT MESS UP

---
+++ GP Final Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody rb;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.VelocityChange);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+       
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        //rb.AddForce(movement * speed);
+
+        transform.position = transform.position + (movement * speed);
+
+
+    }
+}
