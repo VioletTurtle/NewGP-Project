68746cddf2f480d9541fe695083824af69768387
ebotkin12 <ethan.man98@gmail.com>
2019-11-07 00:39:13 -0500

    Added player controller script

--- GP Final Project/Assets/Scripts/PlayerController.cs
+++ GP Final Project/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,20 @@ public class PlayerController : MonoBehaviour
     public float jumpPower;
 
     private Rigidbody rb;
+    private Animator anim;
     public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        anim = GetComponent<Animator>();
+
+        float degrees = 90;
+        Vector3 to = new Vector3(degrees, 0, 0);
+
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+
     }
 
     void OnCollisionStay()
@@ -24,10 +31,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.VelocityChange);
             isGrounded = false;
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
         }
     }
 
@@ -39,10 +53,51 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
 
-        //rb.AddForce(movement * speed);
+        
 
-        transform.position = transform.position + (movement * speed);
+        if(Input.GetButton("PlayerMove"))
+        {
+            transform.position = transform.position + (movement * speed);
 
+            turning(moveHorizontal);
 
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+       
+    }
+
+    void turning(float h)
+    {
+        if (h < 0)
+        {
+            float degrees = -90;
+            Vector3 to = new Vector3(0, degrees, 0);
+
+            transform.eulerAngles = to;
+        }
+        else if(h > 0)
+        {
+            float degrees = 90;
+            Vector3 to = new Vector3(0, degrees, 0);
+
+            transform.eulerAngles = to;
+        }
+    }
+
+    void Animating(float h)
+    {
+        
+           
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f;
+        
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("isWalking", walking);
+        
     }
 }
