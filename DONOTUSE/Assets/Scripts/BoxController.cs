68443e00b85b745ec7f0a1cff57046225f71ea9e
VioletTurtle <violetturtle2002@gmail.com>
2019-11-13 16:52:52 -0500

    Create new Unity with out glitch, use the BoxJumper project

---
+++ DONOTUSE/Assets/Scripts/BoxController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+    }
+}
---
+++ DONOTUSE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody rb;
+    private Animator anim;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        float degrees = 90;
+        Vector3 to = new Vector3(degrees, 0, 0);
+
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.VelocityChange);
+            isGrounded = false;
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+       
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        
+
+        if(Input.GetButton("PlayerMove"))
+        {
+            transform.position = transform.position + (movement * speed);
+
+            turning(moveHorizontal);
+
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+       
+    }
+
+    void turning(float h)
+    {
+        if (h < 0)
+        {
+            float degrees = -90;
+            Vector3 to = new Vector3(0, degrees, 0);
+
+            transform.eulerAngles = to;
+        }
+        else if(h > 0)
+        {
+            float degrees = 90;
+            Vector3 to = new Vector3(0, degrees, 0);
+
+            transform.eulerAngles = to;
+        }
+    }
+
+    void Animating(float h)
+    {
+        
+           
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f;
+        
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("isWalking", walking);
+        
+    }
+}
