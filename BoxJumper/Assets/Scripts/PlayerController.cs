c69a0e04564e046bd83fff2560ff1ed9cf40eac9
ebotkin12 <ethan.man98@gmail.com>
2019-11-16 22:23:57 -0500

    Attempted to fix double jump, with no success.

--- BoxJumper/Assets/Scripts/PlayerController.cs
+++ BoxJumper/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpPower;
+    private float timer;
+    public float jumpDelay;
+    private bool canJump;
 
     private Rigidbody rb;
     private Animator anim;
-    public bool isGrounded;
+    private bool isGrounded;
+    private bool active;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        
+        canJump = true;
         float degrees = 90;
         Vector3 to = new Vector3(degrees, 0, 0);
 
@@ -32,23 +36,19 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void Update()
+   /* void Update()
     {
+        jump();
+        
+        if (active)
+            timer -= Time.deltaTime;    //Subtract the time since last frame from the timer.
+        if (timer < 0)
+            timer = 0;
+        
 
-
-        if (Input.GetKeyDown("space") && isGrounded)
-        {
-            isGrounded = false;
-            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
-            
-            anim.SetBool("isJumping", true);
-        }
-        else
-        {
-            anim.SetBool("isJumping", false);
-            
-        }
-    }
+        //CompleteAction(timer);
+        
+    } */
 
     void FixedUpdate()
     {
@@ -59,8 +59,10 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
 
         
+        jump();
+        
 
-        if(Input.GetButton("PlayerMove"))
+        if (Input.GetButton("PlayerMove"))
         {
             transform.position = transform.position + (movement * speed);
 
@@ -75,6 +77,9 @@ public class PlayerController : MonoBehaviour
 
        
     }
+
+
+    
 
     void turning(float h)
     {
@@ -105,4 +110,34 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isWalking", walking);
         
     }
+
+    void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.VelocityChange);
+            isGrounded = false;
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+
+        }
+    }
+/*
+    public void CompleteAction(float timer)
+    {
+        if (timer > 0)                  //If timer greater than 0, don't complete action
+            return;
+
+        active = false;                 //Set active to be false so timer does not start moving until after action is complete. Remove this if you want timer to restart right away.
+
+        jump();
+
+        timer = jumpDelay;
+
+        active = true;
+    }
+    */
 }
