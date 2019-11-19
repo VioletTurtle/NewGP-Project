a3bdd1fa37e943154265485aabf7a969b6435cd2
VioletTurtle <violetturtle2002@gmail.com>
2019-11-19 14:58:16 -0500

    Fixed box transforms and worked on the start menu.

---
+++ BoxJumper/Assets/Scripts/WinScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    void onTriggerEnter(GameObject o)
+    {
+        if (o.tag == "Player");
+            SceneManager.LoadScene("Win");
+    }
+}
